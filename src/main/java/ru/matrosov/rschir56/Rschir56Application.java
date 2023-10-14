f7fd3df200c487f951b5e46377aa5abdcc1279f8
Danil <matrosovdan@mail.ru>
2023-10-14 23:08:03 +0300

    initial commit

---
+++ src/main/java/ru/matrosov/rschir56/Rschir56Application.java
@@ -0,0 +1,13 @@
+package ru.matrosov.rschir56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rschir56Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rschir56Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/matrosov/rschir56/Rschir56ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matrosov.rschir56;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rschir56ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
